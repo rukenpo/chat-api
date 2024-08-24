18b3137d1c171cb05fefc4237561032c3db71139
rukenpo <164888320+rukenpo@users.noreply.github.com>
2024-08-25 01:32:38 +0800

    Update validation.go

--- relay/util/validation.go
+++ relay/util/validation.go
@@ -1,37 +1,216 @@
 package util
 
 import (
-	"errors"
-	"math"
-	"one-api/relay/constant"
-	"one-api/relay/model"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"one-api/common"
+	"one-api/common/config"
+	relaymodel "one-api/relay/model"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
-func ValidateTextRequest(textRequest *model.GeneralOpenAIRequest, relayMode int) error {
-	if textRequest.MaxTokens < 0 || textRequest.MaxTokens > math.MaxInt32/2 {
-		return errors.New("max_tokens is invalid")
+func ShouldDisableChannel(err *relaymodel.Error, statusCode int) bool {
+	if !config.AutomaticDisableChannelEnabled {
+		return false
 	}
-	if textRequest.Model == "" {
-		return errors.New("model is required")
+	if err == nil {
+		return false
 	}
-	switch relayMode {
-	case constant.RelayModeCompletions:
-		if textRequest.Prompt == "" {
-			return errors.New("field prompt is required")
-		}
-	case constant.RelayModeChatCompletions:
-		if textRequest.Messages == nil || len(textRequest.Messages) == 0 {
-			return errors.New("field messages is required")
-		}
-	case constant.RelayModeEmbeddings:
-	case constant.RelayModeModerations:
-		if textRequest.Input == "" {
-			return errors.New("field input is required")
-		}
-	case constant.RelayModeEdits:
-		if textRequest.Instruction == "" {
-			return errors.New("field instruction is required")
+	if statusCode == http.StatusUnauthorized {
+		return true
+	}
+	if statusCode == http.StatusPreconditionRequired {
+		return true
+	}
+  if statusCode == http.StatusForbidden {
+		return true
+	}
+	switch err.Type {
+	case "insufficient_quota":
+		return true
+	// https://docs.anthropic.com/claude/reference/errors
+	case "authentication_error":
+		return true
+	case "permission_error":
+		return true
+	case "forbidden":
+		return true
+	}
+	if err.Code == "invalid_api_key" || err.Code == "account_deactivated" {
+		return true
+	}
+	if strings.HasPrefix(err.Message, "Your credit balance is too low") { // anthropic
+		return true
+	} else if strings.HasPrefix(err.Message, "This organization has been disabled.") {
+		return true
+	}
+	//if strings.Contains(err.Message, "quota") {
+	//	return true
+	//}
+	if strings.Contains(err.Message, "用户已被封禁") {
+		return true
+	}
+	if strings.Contains(err.Message, "credit") {
+		return true
+	}
+	if strings.Contains(err.Message, "balance") {
+		return true
+	}
+	return false
+}
+
+func ShouldEnableChannel(err error, openAIErr *relaymodel.Error) bool {
+	if !config.AutomaticEnableChannelEnabled {
+		return false
+	}
+	if err != nil {
+		return false
+	}
+	if openAIErr != nil {
+		return false
+	}
+	return true
+}
+
+func GetAPIVersion(c *gin.Context) string {
+	query := c.Request.URL.Query()
+	apiVersion := query.Get("api-version")
+	if apiVersion == "" {
+		apiVersion = c.GetString("api_version")
+	}
+	return apiVersion
+}
+
+type GeneralErrorResponse struct {
+	Error    relaymodel.Error `json:"error"`
+	Message  string           `json:"message"`
+	Msg      string           `json:"msg"`
+	Err      string           `json:"err"`
+	ErrorMsg string           `json:"error_msg"`
+	Header   struct {
+		Message string `json:"message"`
+	} `json:"header"`
+	Response struct {
+		Error struct {
+			Message string `json:"message"`
+		} `json:"error"`
+	} `json:"response"`
+}
+
+func (e GeneralErrorResponse) ToMessage() string {
+	if e.Error.Message != "" {
+		return e.Error.Message
+	}
+	if e.Message != "" {
+		return e.Message
+	}
+	if e.Msg != "" {
+		return e.Msg
+	}
+	if e.Err != "" {
+		return e.Err
+	}
+	if e.ErrorMsg != "" {
+		return e.ErrorMsg
+	}
+	if e.Header.Message != "" {
+		return e.Header.Message
+	}
+	if e.Response.Error.Message != "" {
+		return e.Response.Error.Message
+	}
+	return ""
+}
+
+func RelayErrorHandler(resp *http.Response) (ErrorWithStatusCode *relaymodel.ErrorWithStatusCode) {
+	if resp == nil {
+		return &relaymodel.ErrorWithStatusCode{
+			StatusCode: 500,
+			Error: relaymodel.Error{
+				Message: "resp is nil",
+				Type:    "upstream_error",
+				Code:    "bad_response",
+			},
 		}
 	}
-	return nil
+	ErrorWithStatusCode = &relaymodel.ErrorWithStatusCode{
+		StatusCode: resp.StatusCode,
+		Error: relaymodel.Error{
+			Message: "",
+			Type:    "upstream_error",
+			Code:    "bad_response_status_code",
+			Param:   strconv.Itoa(resp.StatusCode),
+		},
+	}
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	err = resp.Body.Close()
+	if err != nil {
+		return
+	}
+	var errResponse GeneralErrorResponse
+	err = json.Unmarshal(responseBody, &errResponse)
+	if err != nil {
+		return
+	}
+	if errResponse.Error.Message != "" {
+		// OpenAI format error, so we override the default one
+		ErrorWithStatusCode.Error = errResponse.Error
+	} else {
+		ErrorWithStatusCode.Error.Message = errResponse.ToMessage()
+	}
+	if ErrorWithStatusCode.Error.Message == "" {
+		ErrorWithStatusCode.Error.Message = fmt.Sprintf("bad response status code %d", resp.StatusCode)
+	}
+	return
+}
+
+func GetFullRequestURL(baseURL string, requestURL string, channelType int) string {
+	fullRequestURL := fmt.Sprintf("%s%s", baseURL, requestURL)
+
+	if strings.HasPrefix(baseURL, "https://gateway.ai.cloudflare.com") {
+		switch channelType {
+		case common.ChannelTypeOpenAI:
+			fullRequestURL = fmt.Sprintf("%s%s", baseURL, strings.TrimPrefix(requestURL, "/v1"))
+		case common.ChannelTypeAzure:
+			fullRequestURL = fmt.Sprintf("%s%s", baseURL, strings.TrimPrefix(requestURL, "/openai/deployments"))
+		}
+	}
+	return fullRequestURL
+}
+
+func GetAzureAPIVersion(c *gin.Context) string {
+	query := c.Request.URL.Query()
+	apiVersion := query.Get("api-version")
+	if apiVersion == "" {
+		apiVersion = c.GetString(common.ConfigKeyAPIVersion)
+	}
+	return apiVersion
+}
+
+func ResetStatusCode(openaiErr *relaymodel.ErrorWithStatusCode, statusCodeMappingStr string) {
+	if statusCodeMappingStr == "" || statusCodeMappingStr == "{}" {
+		return
+	}
+	statusCodeMapping := make(map[string]string)
+	err := json.Unmarshal([]byte(statusCodeMappingStr), &statusCodeMapping)
+	if err != nil {
+		return
+	}
+	if openaiErr.StatusCode == http.StatusOK {
+		return
+	}
+
+	codeStr := strconv.Itoa(openaiErr.StatusCode)
+	if _, ok := statusCodeMapping[codeStr]; ok {
+		intCode, _ := strconv.Atoi(statusCodeMapping[codeStr])
+		openaiErr.StatusCode = intCode
+	}
 }
