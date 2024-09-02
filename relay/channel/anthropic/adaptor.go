ea3168adb6ffe709565a96871bbea136f2d47912
rukenpo <164888320+rukenpo@users.noreply.github.com>
2024-09-02 14:28:38 +0800

    Update adaptor.go
    cluade重试

--- relay/channel/anthropic/adaptor.go
+++ relay/channel/anthropic/adaptor.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"one-api/common/config"
 	"one-api/relay/channel"
 	"one-api/relay/channel/openai"
 	"one-api/relay/model"
@@ -41,11 +42,10 @@ func (a *Adaptor) ConvertImageRequest(request *model.ImageRequest) (any, error) 
 	}
 	return request, nil
 }
-func (a *Adaptor) ConvertRequest(c *gin.Context, relayMode int, request *model.GeneralOpenAIRequest) (any, error) {
+func (a *Adaptor) ConvertRequest(c *gin.Context, meta *util.RelayMeta, request *model.GeneralOpenAIRequest) (any, error) {
 	if request == nil {
 		return nil, errors.New("request is nil")
 	}
-
 	return ConvertRequest(*request), nil
 
 }
@@ -58,9 +58,23 @@ func (a *Adaptor) DoResponse(c *gin.Context, resp *http.Response, meta *util.Rel
 	if !meta.IsClaude {
 		if meta.IsStream {
 			var responseText string
-			err, _, responseText = StreamHandler(c, resp)
-			usage = openai.ResponseText2Usage(responseText, meta.ActualModelName, meta.PromptTokens)
-
+			err, usage, responseText = StreamHandler(c, resp)
+			if usage == nil {
+				usage = openai.ResponseText2Usage(responseText, meta.ActualModelName, meta.PromptTokens)
+			}
+			if usage.CompletionTokens == 0 {
+				if config.BlankReplyRetryEnabled {
+					return "", nil, &model.ErrorWithStatusCode{
+						Error: model.Error{
+							Message: "No completion tokens generated",
+							Type:    "chat_api_error",
+							Param:   "completionTokens",
+							Code:    500,
+						},
+						StatusCode: 500,
+					}
+				}
+			}
 			aitext = responseText
 		} else {
 			err, usage, aitext = Handler(c, resp, meta.PromptTokens, meta.ActualModelName)
@@ -68,9 +82,23 @@ func (a *Adaptor) DoResponse(c *gin.Context, resp *http.Response, meta *util.Rel
 	} else {
 		if meta.IsStream {
 			var responseText string
-			err, _, responseText = ClaudeStreamHandler(c, resp)
-			usage = openai.ResponseText2Usage(responseText, meta.ActualModelName, meta.PromptTokens)
-
+			err, usage, responseText = ClaudeStreamHandler(c, resp)
+			if usage == nil {
+				usage = openai.ResponseText2Usage(responseText, meta.ActualModelName, meta.PromptTokens)
+			}
+			if usage.CompletionTokens == 0 {
+				if config.BlankReplyRetryEnabled {
+					return "", nil, &model.ErrorWithStatusCode{
+						Error: model.Error{
+							Message: "No completion tokens generated",
+							Type:    "chat_api_error",
+							Param:   "completionTokens",
+							Code:    500,
+						},
+						StatusCode: 500,
+					}
+				}
+			}
 			aitext = responseText
 		} else {
 			err, usage, aitext = ClaudeHandler(c, resp, meta.PromptTokens, meta.ActualModelName)
