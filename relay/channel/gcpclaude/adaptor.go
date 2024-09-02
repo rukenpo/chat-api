9e4c17ec24f4b05eed56ced134247edb659f8dcf
rukenpo <164888320+rukenpo@users.noreply.github.com>
2024-09-02 14:47:44 +0800

    Update adaptor.go

--- relay/channel/gcpclaude/adaptor.go
+++ relay/channel/gcpclaude/adaptor.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net/http"
 
+	"one-api/common/config"
 	"one-api/relay/channel"
 	"one-api/relay/channel/anthropic"
 	"one-api/relay/channel/openai"
@@ -71,4 +72,87 @@ func (a *Adaptor) GetRequestURL(meta *util.RelayMeta) (string, error) {
 }
 
 func (a *Adaptor) SetupRequestHeader(c *gin.Context, req *http.Request, meta *util.RelayMeta) error {
-	channel.SetupCommonRequestHeader(c,
+	channel.SetupCommonRequestHeader(c, req, meta)
+	req.Header.Set("Authorization", "Bearer "+meta.APIKey)
+	req.Header.Del("anthropic-version")
+	return nil
+}
+
+func (a *Adaptor) ConvertImageRequest(request *model.ImageRequest) (any, error) {
+	if request == nil {
+		return nil, errors.New("request is nil")
+	}
+	return request, nil
+}
+
+func (a *Adaptor) ConvertRequest(c *gin.Context, meta *util.RelayMeta, request *model.GeneralOpenAIRequest) (any, error) {
+	if request == nil {
+		return nil, errors.New("request is nil")
+	}
+	return ConvertRequest(*request), nil
+}
+
+func (a *Adaptor) DoRequest(c *gin.Context, meta *util.RelayMeta, requestBody io.Reader) (*http.Response, error) {
+	return channel.DoRequestHelper(a, c, meta, requestBody)
+}
+
+func (a *Adaptor) DoResponse(c *gin.Context, resp *http.Response, meta *util.RelayMeta) (aitext string, usage *model.Usage, err *model.ErrorWithStatusCode) {
+	if !meta.IsClaude {
+		if meta.IsStream {
+			var responseText string
+			err, usage, responseText = anthropic.StreamHandler(c, resp)
+			if usage == nil {
+				usage = openai.ResponseText2Usage(responseText, meta.ActualModelName, meta.PromptTokens)
+			}
+			if usage.CompletionTokens == 0 {
+				if config.BlankReplyRetryEnabled {
+					return "", nil, &model.ErrorWithStatusCode{
+						Error: model.Error{
+							Message: "No completion tokens generated",
+							Type:    "chat_api_error",
+							Param:   "completionTokens",
+							Code:    500,
+						},
+						StatusCode: 500,
+					}
+				}
+			}
+			aitext = responseText
+		} else {
+			err, usage, aitext = anthropic.Handler(c, resp, meta.PromptTokens, meta.ActualModelName)
+		}
+	} else {
+		if meta.IsStream {
+			var responseText string
+			err, usage, responseText = anthropic.ClaudeStreamHandler(c, resp)
+			if usage == nil {
+				usage = openai.ResponseText2Usage(responseText, meta.ActualModelName, meta.PromptTokens)
+			}
+			if usage.CompletionTokens == 0 {
+				if config.BlankReplyRetryEnabled {
+					return "", nil, &model.ErrorWithStatusCode{
+						Error: model.Error{
+							Message: "No completion tokens generated",
+							Type:    "chat_api_error",
+							Param:   "completionTokens",
+							Code:    500,
+						},
+						StatusCode: 500,
+					}
+				}
+			}
+			aitext = responseText
+		} else {
+			err, usage, aitext = anthropic.ClaudeHandler(c, resp, meta.PromptTokens, meta.ActualModelName)
+		}
+	}
+	return
+}
+
+func (a *Adaptor) GetModelList() []string {
+	return anthropic.ModelList
+}
+
+func (a *Adaptor) GetChannelName() string {
+	return "Anthropic Claude"
+}
